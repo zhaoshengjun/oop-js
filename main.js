099e8a5c37ba743c975ba3d7704f043b63ee83c5
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-18 22:28:24 +1000

    factory vs constructor

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+// Factory vs. Constructor
+
+// Factory function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log("Draw ...");
+    }
+  };
+}
+const circleCreatedByFactory = createCircle(4);
+circleCreatedByFactory.draw();
+
+// Constructor
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw ...");
+  };
+}
+const circleCreatedByConstructor = new Circle(2);
+circleCreatedByConstructor.draw();
