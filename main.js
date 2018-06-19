9af92c52737c5361fce10c6f295ecc607422b7e0
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-19 22:51:13 +1000

    function = object

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
-// Factory vs. Constructor
-
-// Factory function
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function() {
-      console.log("Draw ...");
-    }
-  };
-}
-const circleCreatedByFactory = createCircle(4);
-circleCreatedByFactory.draw();
-
-// Constructor
 function Circle(radius) {
   this.radius = radius;
   this.draw = function() {
     console.log("draw ...");
   };
 }
-const circleCreatedByConstructor = new Circle(2);
-circleCreatedByConstructor.draw();
 
-console.log(circleCreatedByFactory.constructor);
-console.log(circleCreatedByConstructor.constructor);
+console.log(Circle.name); // function name
+console.log(Circle.length); // function parameters
+console.log(Circle.constructor); // function constructor
