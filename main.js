fecbc07be327f1af38835bd4072a6640108e0ee9
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-19 22:54:31 +1000

    Function

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ function Circle(radius) {
 console.log(Circle.name); // function name
 console.log(Circle.length); // function parameters
 console.log(Circle.constructor); // function constructor
+
+const CircleFn = new Function(
+  "radius", // function parameter
+  `
+this.radius = radius;
+this.draw = function() {
+  console.log("draw ...");
+};` // function body
+);
+
+let circle = new CircleFn(1);
+console.log(circle);
