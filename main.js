cb5ba6acd1d197bdec5bde2e410cf5b71837a4cb
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-20 23:23:26 +1000

    use private var and functions

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function Circle(radius) {
+  let defaultLocation = { x: 0, y: 0 };
   this.radius = radius;
-  this.defaultLocation = { x: 0, y: 0 };
-  this.computeOptimumLocation = function() {
+  let computeOptimumLocation = function() {
     // use default location to do calculation
+    defaultLocation.x += 10;
   };
   this.draw = function() {
-    this.computeOptimumLocation();
+    computeOptimumLocation();
     console.log("draw");
   };
 }
 const circle = new Circle(10);
-circle.defaultLocation = false;
-circle.draw(); // this will create error
+circle.draw();
