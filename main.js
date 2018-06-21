26b418657f462ab8f33cd3a0829dc804a4059af7
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-21 22:39:17 +1000

    use defineProperty to set a getter

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function Circle(radius) {
   let defaultLocation = { x: 0, y: 0 };
   this.radius = radius;
-  let computeOptimumLocation = function() {
-    // use default location to do calculation
-    defaultLocation.x += 10;
-  };
   this.draw = function() {
-    computeOptimumLocation();
     console.log("draw");
   };
+
+  Object.defineProperty(this, "defaultLocation", {
+    get: function() {
+      return defaultLocation;
+    }
+  });
 }
 const circle = new Circle(10);
 circle.draw();
