61855112b1cdf69c543604fd1430edceb2ee0315
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-22 21:53:58 +1000

    prototype

--- main.js
+++ main.js
@@ -1,38 +1,4 @@
-function StopWatch() {
-  let startTime,
-    stopTime,
-    status,
-    duration = 0;
-  this.start = function() {
-    if (status === "started") {
-      throw new Error("Stop watch has already started.");
-    }
-    status = "started";
-    startTime = Date.now();
-  };
+let x = {};
+let y = {};
 
-  this.stop = function() {
-    if (status === "stopped") {
-      throw new Error("Stop watch has already stopped.");
-    } else if (status === "started") {
-      status = "stopped";
-      stopTime = Date.now();
-      duration = duration + (stopTime - startTime) / 1000;
-    } else {
-      throw new Error("Stop watch has not started yet.");
-    }
-  };
-
-  this.reset = function() {
-    status = null;
-    startTime = null;
-    stopTime = null;
-    duration = 0;
-  };
-
-  Object.defineProperty(this, "duration", {
-    get: function() {
-      return duration + "s";
-    }
-  });
-}
+Object.getPrototypeOf(x) === Object.getPrototypeOf(y); //return true
