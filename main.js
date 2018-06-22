55d367f4a0d1c29859aaf715329aaf73e1944b7a
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-22 22:23:39 +1000

    update prototype

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-let myObj;
-Object.getPrototypeOf(myObj) === myObj.__proto__;
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype.draw = function() {
+  console.log("draw");
+};
+
+let c1 = new Circle(1);
+let c2 = new Circle(2);
+// c1 and c2 has only radius property, but you can call draw method because it's defined in the super class.
