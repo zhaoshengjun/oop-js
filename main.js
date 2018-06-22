33729ebc658494803dc0973716305b375121f82d
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-22 22:14:03 +1000

    property descriptor

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let person = { name: "John" };
 
-person.toString();
+let personBase = Object.getPrototypeOf(person);
 
-for (let key in person) console.log(key);
-
-Object.keys(person);
+let des = Object.getOwnPropertyDescriptor(personBase, "toString");
+console.log(des);
+// configuration: true; enumerable: false, writable: true
