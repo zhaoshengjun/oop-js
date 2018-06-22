495c381721b61364913f96880e12b387d9bc48d5
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-22 22:15:50 +1000

    enumerable

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let person = { name: "John" };
 
 Object.defineProperty(person, "name", {
-  writable: false
+  writable: false,
+  enumerable: false
 });
 
 person.name = "Josh";
 console.log(person.name);
+console.log(Object.keys(person));
